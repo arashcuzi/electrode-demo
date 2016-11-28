8652c23f3b161d58d6aa23dfa75f9e155038463b
Shawn Petros <arashcuzi@gmail.com>
2016-11-28 17:30:45 -0600

    some changes

--- client/routes.jsx
+++ client/routes.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import {Route} from "react-router";
-import {Home} from "./components/home";
-import Hello from "./components/hello";
-import World from "./components/world";
+import Home from "./components/home";
 
-export const routes = [
-  <Route path="/" component={Home}/>,
-  <Route path="hello" component={Hello}/>,
-  <Route path="world" component={World}/>
-];
+export const routes = (
+  <Route path="/" component={Home}/>
+);
